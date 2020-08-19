429db5bf505d41a12a6aa357a8f8b51eaf2515df
vyteking <65077361+vyteking@users.noreply.github.com>
2020-08-19 17:01:45 +0900

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,15 @@
+import random
+
 def first():
   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
+  last = len(quotes) - 1
+  rdn = random.randint(0, last)
   f.close()
 
-  print(quotes[0])
+  print(quotes[rnd])
 
 if __name__== "__main__":
   first()
